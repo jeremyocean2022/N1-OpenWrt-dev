1ed5803fc67f58606c5db4f9704c1b4739bd9425
jeremyocean2022 <121137854+jeremyocean2022@users.noreply.github.com>
2025-12-09 19:28:03 +0800

    Clean up package cloning in diy.sh
    Removed unnecessary Golang package clone and cleanup.

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -8,11 +8,9 @@ git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 clone/package/luci-app-mosdns
 git clone https://github.com/sbwml/v2ray-geodata clone/package/v2ray-geodata
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/packages/lang/golang
 
 cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall clone/package/luci-app-mosdns feeds/luci/applications/
 cp -rf clone/package/v2ray-geodata feeds/packages/net/
