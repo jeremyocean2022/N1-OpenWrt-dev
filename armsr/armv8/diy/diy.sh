423617d87af639b1f3eee196137b9d4634df59fa
jeremyocean2022 <121137854+jeremyocean2022@users.noreply.github.com>
2025-12-09 18:37:38 +0800

    Add mosdns package and update copy commands

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -6,10 +6,11 @@ patch -p1 -f < $(dirname "$0")/luci.patch
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+git clone https://github.com/sbwml/luci-app-mosdns  --depth=1 clone/mosdns
 
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
-cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall clone/mosdns/luci-app-mosdns feeds/luci/applications/
 
 # Clean packages
 rm -rf clone
