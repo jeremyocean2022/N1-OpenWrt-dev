e9ec5f31c65b892f936cfa8c8783a02396599395
jeremyocean2022 <121137854+jeremyocean2022@users.noreply.github.com>
2025-12-09 19:06:58 +0800

    Refactor package cloning and update paths in diy.sh
    Updated package cloning and copying paths in diy.sh.

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -6,11 +6,14 @@ patch -p1 -f < $(dirname "$0")/luci.patch
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
-git clone https://github.com/sbwml/luci-app-mosdns  --depth=1 clone/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 clone/package/luci-app-mosdns
+git clone https://github.com/sbwml/v2ray-geodata clone/package/v2ray-geodata
 
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
-cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall clone/mosdns/luci-app-mosdns feeds/luci/applications/
+
+cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall clone/package/luci-app-mosdns feeds/luci/applications/
+cp -rf clone/package/v2ray-geodata feeds/packages/net/
 
 # Clean packages
 rm -rf clone
